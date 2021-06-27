d87aa02a1ea25daadffa1bc1cb6b3f76914e9efe
krishpranav <krisna.pranav@gmail.com>
2021-06-27 16:55:40 +0530

    [+] assembly [+]

--- Firefoxthief/Properties/AssemblyInfo.cs
+++ Firefoxthief/Properties/AssemblyInfo.cs
@@ -1,3 +1,12 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Firefox Stealer")]
+[assembly: AssemblyDescription("https://github.com/LimerBoy/FireFox-Thief")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Stealer")]
+[assembly: AssemblyCopyright("LimerBoy")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]

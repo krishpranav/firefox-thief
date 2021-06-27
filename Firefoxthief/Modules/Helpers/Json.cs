ef72387a0f7f420ef5a147414893e37f6142223d
krishpranav <krisna.pranav@gmail.com>
2021-06-27 17:01:25 +0530

    [+] json [+ ]

---
+++ Firefoxthief/Modules/Helpers/Json.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace Stealer.Helpers
+{
+    internal sealed class Json
+    {
+        public string Data;
+        public Json(string Data)
+        {
+            this.Data = data;
+        }
+    }
+}

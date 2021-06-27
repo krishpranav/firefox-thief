e2731c9aa51f30d844426fe5236983cf0cfb7a84
krishpranav <krisna.pranav@gmail.com>
2021-06-27 16:57:21 +0530

    [+] password struct [+]

---
+++ Firefoxthief/Modules/Helpers/Common.cs
@@ -0,0 +1,12 @@
+namespace Stealer.Helpers
+{
+    internal sealed class Common
+    {
+        public struct Password
+        {
+            public string sUrl { get; set; }
+            public string sUsername { get; set; }
+            public string sPassword { get; set; }
+        }
+    }
+}

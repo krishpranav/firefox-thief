5039eca99522b7ae8062a61b9e5131021df5430b
krishpranav <krisna.pranav@gmail.com>
2021-06-27 16:56:00 +0530

    [+] assembly [+]

--- Firefoxthief/Properties/AssemblyInfo.cs
+++ Firefoxthief/Properties/AssemblyInfo.cs
@@ -10,3 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("LimerBoy")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d0615a53-cac0-4ea9-bc41-14e6c2233f19")]
